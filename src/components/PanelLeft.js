a5d2164a2c8b852b9d3f71ce3f5a6988a9d1c790
skxlpv <olexandertym0000@gmail.com>
2023-06-28 13:10:26 +0300

    Updated Home page components

---
+++ src/components/PanelLeft.js
@@ -0,0 +1,21 @@
+import React from "react";
+import '../styles/Panel.css'
+
+export default function PanelLeft(props){
+
+    let hasImage = props.imgPath? true : false
+    return(
+        <div className="home-content-panel">
+            <div className="home-content-left">
+                <h1>{props.title}</h1>
+                <p className="home-paragraph-left">
+                    {props.description}
+                </p>
+            </div>
+
+            <div className="home-content-right">
+                {hasImage? <img src={props.imgPath}></img> : null}
+            </div>
+        </div>
+    )
+}
